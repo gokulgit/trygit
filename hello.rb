5b058e1237cbdda61264258b9ec38782d4155859
Gokul Andavar <gokul.a@gmail.com>
2011-01-08 19:20:35 +0800

    This is for adding the book name

--- hello.rb
+++ hello.rb
@@ -1,12 +1,13 @@
 class Book
-  attr_accessor :author, :publication, :price
+  attr_accessor :author, :publication, :price, :name
   def initialize(author, publication, price)
     @author = author
     @publication = publication
     @price = price
+    @name = name
   end
   def to_s
-    @author.to_s + "-" + @publication.to_s + "-" + @price.to_s
+    @name.to_s + "-" + @author.to_s + "-" + @publication.to_s + "-" + @price.to_s
   end
 end
 
